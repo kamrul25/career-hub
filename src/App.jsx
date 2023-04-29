5ee074583fead8a0046170cbb6b80532d37fd996
kamrul25 <kamrul.coder01@gmail.com>
2023-04-30 02:55:17 +0600

    handel error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="flex flex-col justify-between gap-28">
-      {/* heding */}
-      <Outlet />
-      <Footer />
-    </div>
+      <Header></Header>
+      <Outlet></Outlet>
+      <Footer></Footer> 
+
+      </div>
   );
 }
 
--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import { useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const { error, status } = useRouteError();
+
+  return (
+    <div className="flex flex-col justify-center items-center mt-[35vh] text-red-600">
+      <h1 className="text-3xl font-bold mb-5">Oops!</h1>
+      <p className="text-3xl font-bold mb-5">Sorry, an unexpected error has occurred.</p>
+      <p className="text-3xl font-bold mb-5">
+        <i>{error.statusText || error.message}</i>
+      </p>
+      <p className="text-3xl font-bold mb-5">
+        <i>{status}</i>
+      </p>
+    </div>
+  );
 };
 
 export default ErrorPage;
