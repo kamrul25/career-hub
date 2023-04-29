0747785147728ad3158ab6120379300b3caa243a
kamrul25 <kamrul.coder01@gmail.com>
2023-04-30 04:01:54 +0600

    create navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,142 @@
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import {
+  BoltIcon,
+  Bars3BottomRightIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/solid";
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  return (
+    <div className=" px-4 py-5  sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+      <div className="flex items-center justify-between">
+        <h2 className="text-3xl font-extrabold">Job Hub</h2>
+        {/* Nav Items Section */}
+        <ul className="items-center hidden space-x-8 lg:flex">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "active" : "default")}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/statistics"
+              className={({ isActive }) => (isActive ? "active" : "default")}
+            >
+              Statistics
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/applied"
+              className={({ isActive }) => (isActive ? "active" : "default")}
+            >
+              Applied Jobs
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/blog"
+              className={({ isActive }) => (isActive ? "active" : "default")}
+            >
+              Blog
+            </NavLink>
+          </li>
+        </ul>
+        <Link
+          to="/start-applying"
+          className="btn h-14 py-4 px-6 rounded-lg lg:block hidden"
+        >
+          Start Applying
+        </Link>
+        {/* Mobile Navbar Section */}
+        <div className="lg:hidden">
+          {/* Dropdown Open Button */}
+          <button
+            aria-label="Open Menu"
+            title="Open Menu"
+            onClick={() => setIsMenuOpen(true)}
+          >
+            <Bars3BottomRightIcon className="w-5 text-gray-600" />
+          </button>
+          {isMenuOpen && (
+            <div className="absolute top-0 left-0 w-full z-10">
+              <div className="p-5 bg-white border rounded shadow-sm">
+                <div className="flex items-center justify-between mb-4">
+                  <h1 className="text-3xl font-extrabold">Job Hub</h1>
+                  {/* Dropdown menu close button */}
+                  <div>
+                    <button
+                      aria-label="Close Menu"
+                      title="Close Menu"
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      <XMarkIcon className="w-5 text-gray-600" />
+                    </button>
+                  </div>
+                </div>
+                {/* Mobile Nav Items Section */}
+                <ul className="space-x-8">
+                  <li>
+                    <NavLink
+                      to="/"
+                      className={({ isActive }) =>
+                        isActive ? "active" : "default"
+                      }
+                    >
+                      Home
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to="/statistics"
+                      className={({ isActive }) =>
+                        isActive ? "active" : "default"
+                      }
+                    >
+                      Statistics
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to="/applied"
+                      className={({ isActive }) =>
+                        isActive ? "active" : "default"
+                      }
+                    >
+                      Applied Jobs
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to="/blog"
+                      className={({ isActive }) =>
+                        isActive ? "active" : "default"
+                      }
+                    >
+                      Blog
+                    </NavLink>
+                  </li>
+                  <li className="mt-4">
+                    <Link
+                      to="/start-applying"
+                      className="btn  py-2 px-3 rounded-lg lg:hidden "
+                    >
+                      Start Applying
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
