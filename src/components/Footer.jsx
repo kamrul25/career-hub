1c073314a01de9bf9a7561a80e3e41652ec1ac31
kamrul25 <kamrul.coder01@gmail.com>
2023-04-30 08:24:19 +0600

    create easiest footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-
 const Footer = () => {
     return (
-        <footer >
-      
-        </footer>
-    );
-};
-
-export default Footer;
+      <footer className='px-4 divide-y  text-gray-800 relative bottom-0 left-0'>
+        <div className='py-6 text-sm text-center text-gray-400'>
+          © 2023 Job Hub Inc. All rights reserved.
+        </div>
+      </footer>
+    )
+  }
+  
+  export default Footer
