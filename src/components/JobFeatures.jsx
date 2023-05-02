7da654e13425abb542e9c96fb94d92575a5ef4cd
kamrul25 <kamrul.coder01@gmail.com>
2023-05-02 12:01:33 +0600

    added see all and less button

--- src/components/JobFeatures.jsx
+++ src/components/JobFeatures.jsx
@@ -4,22 +4,40 @@ import { JobsContext } from "../App";
 
 const JobFeatures = () => {
   const jobFeatures = useContext(JobsContext || []);
-  const [fold, setFold] = useState(true);
+  const [fold, setFold] = useState(false);
 
   return (
     <div>
       <div className="flex flex-col  items-center ">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-3 py-10 px-4 md:px-24 lg:p-16">
-          {jobFeatures.map((feature) => (
-            <JobFeature key={feature.id} feature={feature}></JobFeature>
-          ))}
-        </div>
-        <button
-          className="btn rounded-lg py-4 px-6"
-          onClick={() => setFold(!fold)}
-        >
-          See All Jobs
-        </button>
+        {fold ? (
+          <>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-3 py-10 px-4 md:px-24 lg:p-16">
+            {jobFeatures.map((feature) => (
+              <JobFeature key={feature.id} feature={feature}></JobFeature>
+            ))}
+          </div>
+          <button
+              className="btn rounded-lg py-4 px-6"
+              onClick={() => setFold(!fold)}
+            >
+              See Less
+            </button>
+          </>
+        ) : (
+          <>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-3 py-10 px-4 md:px-24 lg:p-16">
+              {jobFeatures.slice(0, 4).map((feature) => (
+                <JobFeature key={feature.id} feature={feature}></JobFeature>
+              ))}
+            </div>
+            <button
+              className="btn rounded-lg py-4 px-6"
+              onClick={() => setFold(!fold)}
+            >
+              See All Jobs
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
